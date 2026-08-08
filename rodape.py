73f31c192d1fab1a6c67d66309cc70a00e69efc8
Fábio Garcia <garciafabioha@gmail.com>
2026-08-08 10:13:21 -0300

    Ajustado a Página de Rodapé

--- rodape.py
+++ rodape.py
@@ -11,32 +11,26 @@ def exibir_rodape():
             text-align: center;
             font-size: 14px;
             color: #8b949e;
-            line-height: 1.8;
-            padding-top: 10px;
-            padding-bottom: 20px;
+            line-height: 1.6;
+            padding-top: 8px;
+            padding-bottom: 15px;
         ">
 
-        <strong>Conecta Bairro® — © 2026 Garcia Consultoria &amp; T.I LTDA.
-        Todos os direitos reservados.</strong>
+        <strong>
+            Conecta Bairro® — © 2026 Garcia Consultoria &amp; T.I LTDA.
+            Todos os direitos reservados.
+        </strong>
 
         <br><br>
 
-        <strong>Empresa:</strong>
-        Garcia Consultoria &amp; T.I LTDA<br>
-
-        <strong>CNPJ:</strong>
-        66.495.350/0001-84<br>
-
+        <strong>Empresa:</strong> Garcia Consultoria &amp; T.I LTDA<br>
+        <strong>CNPJ:</strong> 66.495.350/0001-84<br>
         <strong>Contato:</strong>
         <a href="mailto:garciafabioha@gmail.com">
             garciafabioha@gmail.com
         </a><br>
-
-        <strong>Celular:</strong>
-        (44) 9 9722-0216<br>
-
-        <strong>Cidade:</strong>
-        Umuarama - PR
+        <strong>Celular:</strong> (44) 9 9722-0216<br>
+        <strong>Cidade:</strong> Umuarama - PR
 
         </div>
         """,
